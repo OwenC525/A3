4ba764ee959e17be4d95cfe32fdb8cef03b7add4
Owen Chiu <owencc8@gmail.com>
2025-10-07 15:48:38 -0400

    initial

---
+++ Hopscotch.java
@@ -0,0 +1,21 @@
+public class Hopscotch {
+    /*************************************************************************
+ *  Compilation:  javac Hopscotch.java
+ *  Execution:    java Hopscotch size
+ *
+ *  @author Ram Buditi
+ *
+    *************************************************************************/
+    
+    /* Prints all rectangles on the hopscotch path with first 
+       the center rectangles, then the left rectangles,
+       and lastly the right rectangles as a sequence of
+       integers.
+
+       size: The size of the hopscotch path(the total number of rectangles)
+     */
+    public static void main(String[] args) {
+        //WRITE YOUR CODE HERE
+    }
+
+}
---
+++ RUChange.java
@@ -0,0 +1,33 @@
+/**
+ * Write the RUChange program inside the main method  according to the 
+ * assignment description.
+ * 
+ * You should read the argument (number of cents due) as an int and then calculate 
+ * the least number of coins needed to achieve that amount. Although your able to 
+ * only use arithmetic operations to complete the assignment, you should use loops 
+ * and conditionals as required by the assignment. You will also have to print out 
+ * the number of quarters, dimes, nickles, and pennies needed to obtain that amount 
+ * of change.
+ * 
+ * To compile:
+ *        javac RUChange.java
+ * To execute:
+ *        java RUChange 76
+ * 
+ * DO NOT change the class name
+ * DO NOT use System.exit()
+ * DO NOT change add import statements
+ * DO NOT add project statement
+ * 
+ * @param args args[0] is the amount in cents to make change for
+ * @author Ved Patel
+ * @author Anna Lu
+ */
+
+
+public class RUChange {
+    public static void main( String [] args ){
+        int centsDue = Integer.parseInt(args[0]);
+        
+    }
+ }
