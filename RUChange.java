1c9c2d3ea94870a9b7bb5ec9446e071d8cfbe3ff
Owen Chiu <owencc8@gmail.com>
2025-10-07 16:54:55 -0400

    almost done; small errors

--- RUChange.java
+++ RUChange.java
@@ -28,6 +28,25 @@
 public class RUChange {
     public static void main( String [] args ){
         int centsDue = Integer.parseInt(args[0]);
-        
+        int quarters = 0;
+        int dimes = 0;
+        int nickels = 0;
+        int pennies = 0;
+
+        if (centsDue < 0){
+            System.out.println("Invalid input!");
+        }
+
+        quarters = (centsDue / 25);
+        dimes = ((centsDue % 25) / 10);
+        nickels = ((centsDue % 10) / 5);
+        pennies = ((centsDue % 5) / 1);
+
+        System.out.println("Quarters: " + quarters);
+        System.out.println("Dimes: " + dimes);
+        System.out.println("Nickels: " + nickels);
+        System.out.println("Pennies: "+ pennies);
+
+
     }
  }
